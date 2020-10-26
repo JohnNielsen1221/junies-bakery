879f1c6815e00d3ff042780ce4ddb3c89432ad35
John Nielsen <johnhayesnielsen@gmail.com>
2020-10-26 16:28:27 -0600

    started bulding menu layout

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -7,6 +7,14 @@ function Menu() {
     return (
         <div>
             <h1>Weekly Menu</h1>
+            <h3>Menu Item 1</h3>
+            <p>Description of Item</p>
+            <h3>Menu Item 2</h3>
+            <p>Description of Item</p>
+            <h3>Menu Item 3</h3>
+            <p>Description of Item</p>
+            <h3>Menu Item 4</h3>
+            <p>Description of Item</p>
             
         </div>
     );
