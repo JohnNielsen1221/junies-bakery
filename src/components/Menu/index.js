53bec08952a3d00e6f6edac12a71d05d55c68d27
John Nielsen <johnhayesnielsen@gmail.com>
2020-10-26 16:35:56 -0600

    added input fields

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -5,6 +5,7 @@ import React from 'react';
 function Menu() {
 
     return (
+        <>
         <div>
             <h1>Weekly Menu</h1>
             <h3>Menu Item 1</h3>
@@ -15,8 +16,18 @@ function Menu() {
             <p>Description of Item</p>
             <h3>Menu Item 4</h3>
             <p>Description of Item</p>
-            
         </div>
+        <div>
+            <p>How many ___ would you like?</p>
+            <input type='number'></input>
+            <p>How many ___ would you like?</p>
+            <input type='number'></input>
+            <p>How many ___ would you like?</p>
+            <input type='number'></input>
+            <p>How many ___ would you like?</p>
+            <input type='number'></input>
+        </div>
+        </>
     );
 }
 
