6b7655923bd37d3b3066bf4c9025d5ac8f4bfe0a
John Nielsen <johnhayesnielsen@gmail.com>
2020-10-30 10:59:21 -0600

    updated footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
-            <h3 className='footer-head'>Don't stop believin'...</h3>
+            <h3 className='footer-head'>Follow me on Instagram for the latest menu's and promotions!</h3>
             <a href="https://github.com/JohnNielsen1221"  target="_blank" rel="noopener noreferrer"><img src="https://img.icons8.com/nolan/64/github.png" alt="Github"/></a>
             <a href="https://www.linkedin.com/in/johnhayesnielsen/"  target="_blank" rel="noopener noreferrer"><img src="https://img.icons8.com/nolan/64/linkedin.png" alt="LinkedIn"/></a>
             <a href="https://www.facebook.com/john.h.nielsen.7"  target="_blank" rel="noopener noreferrer"><img src="https://img.icons8.com/nolan/64/facebook-new.png" alt="Facebook"/></a>
